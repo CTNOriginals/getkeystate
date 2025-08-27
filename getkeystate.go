ae648695b14e8a11611df98de41c54f62214afd4
CTN <info@ctn-originals.com>
2025-08-27 19:25:30 +0200

    Removed the KeyState type
    It is wrong to return a custom type by default instead of just a common type like int, with int, the users can use the return type of the function in a bit more ways off the bet.

--- getkeystate.go
+++ getkeystate.go
@@ -2,28 +2,8 @@
 
 package getkeystate
 
-// #include <stdio.h>
+// #include "./main.h"
 import "C"
-
-// #region KeyState
-
-// A 2 bit int representing a key state.
-//
-// The right most bit represents wether it is currently held down (active).
-// The left most bit represents wether the key is currently toggled.
-type KeyState int
-
-// Returns wether this key state signals if the key is being held down.
-func (this KeyState) IsActive() bool {
-	return (this & 2) != 0
-}
-
-// Returns wether this key state signals if the key is toggled.
-func (this KeyState) IsToggled() bool {
-	return (this & 1) != 0
-}
-
-//#endregion
 
 // Get the current state of key.
 // key accepts the raw key code interger for VK_BUTTONS listed here: https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes.
@@ -33,17 +13,31 @@ func (this KeyState) IsToggled() bool {
 // The left most bit represents wether the key is currently toggled.
 //
 // All keys will return a toggled on/off value, but this functionality is mostly useful for keys like CapsLock.
-func GetKeyState(key int) KeyState {
+func GetKeyState(key int) int {
 	var state = C.C_GetKeyState(C.int(key))
 
 	switch state {
 	case 1: // Toggled
-		return KeyState(1)
+		return 0b01
 	case -128: // Active >| not toggled
-		return KeyState(2)
+		return 0b10
 	case -127: // Active >| toggled
-		return KeyState(3)
+		return 0b11
 	default: // idle
-		return KeyState(0)
+		return 0b00
 	}
+}
+
+// Takes a keyState that is returned by GetKeyState().
+//
+// Returns wether this key state signals if the key is being held down.
+func IsActive(keyState int) bool {
+	return (keyState & 2) != 0
+}
+
+// Takes a keyState that is returned by GetKeyState().
+//
+// Returns wether this key state signals if the key is toggled.
+func IsToggled(keyState int) bool {
+	return (keyState & 1) != 0
 }
